37b01e787d88a0e7f31acfecf10a177955a601f3
NAVEEN MABBU <95521452+naveenmabbu@users.noreply.github.com>
2022-01-31 20:33:00 +0530

    Update gambler variables.cs

--- gambler variables.cs
+++ gambler variables.cs
@@ -21,8 +21,8 @@ namespace gambler_simulation
         public static int Gamestarted()
         {
             Random random = new Random();
-            int BET = random.Next(0, 2);
-            if (BET == 1)
+            int Bet = random.Next(0, 2);
+            if (Bet == 1)
             {
                 stakeValue = STAKE + BET;
                 Console.WriteLine("Gambler Won the Game" + stakeValue);
