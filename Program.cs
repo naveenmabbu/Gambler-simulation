e1c6f3f5c575f823427abc83338bc8b5b3c5d874
NAVEEN MABBU <95521452+naveenmabbu@users.noreply.github.com>
2022-01-31 18:55:56 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+using gambler_simulation;
+
+Console.WriteLine("Hello, World!");
+gambler_variables.Main();
---
+++ gambler variables.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gambler_simulation
+{
+    public class gambler_variables
+    {
+       
+        public const int STAKE = 100;
+        public const int BET = 1;
+        public static void Main()
+        {
+            Console.WriteLine("Welcome to GamblerSimulation program");
+        }
+    }
+}
+
+
